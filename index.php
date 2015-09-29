f0c6e30ca51494b8e9e34a97fd898be2caaaba12
gravityfox <gravityreallyhatesme@gmail.com>
2015-09-28 23:03:45 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fox
+ * Date: 8/1/2015
+ * Time: 1:26 PM
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="styles/loader.css">
+    <link rel="stylesheet" type="text/css" href="styles/index.css">
+    <link rel="stylesheet" type="text/css" href="styles/animations.css">
+    <link rel="apple-touch-icon" sizes="57x57" href="fox-icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="fox-icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="fox-icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="fox-icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="fox-icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="fox-icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="fox-icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="fox-icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="fox-icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="fox-icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="fox-icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="fox-icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="fox-icons/favicon-16x16.png">
+    <link rel="manifest" href="/src/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="icons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+</head>
+
+<body>
+<div id="loader-wrapper">
+    <div id="loader"></div>
+    <div class="loader-section"></div>
+</div>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+<div id="contentContainer">
+    <header class="topBar">
+        <h1 id="titleText"> Hi. I'm Mike.</h1>
+    </header>
+    <div id="back"></div>
+    <div id="content">
+        <h1>Here Is a Bit of Me.</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur corporis ducimus explicabo fugiat
+            ipsum,
+            laudantium, minus nisi officia saepe soluta vero voluptatibus. Aspernatur at aut dolor earum, esse harum
+            hic
+            iusto laborum magnam modi molestiae molestias, officiis quas similique sint tempore voluptates. Alias
+            assumenda
+            dolores iusto nam necessitatibus. Accusamus accusantium culpa, eaque exercitationem id laborum minima
+            neque,
+            nobis quaerat quos saepe voluptas voluptatem. Accusamus aliquid harum molestiae neque possimus
+            praesentium
+            reprehenderit sit tempore, velit voluptate. Beatae deleniti esse excepturi fugiat laudantium nesciunt
+            quo
+            reiciendis soluta voluptate voluptates. Accusamus animi at deleniti id, inventore ipsum provident quia
+            repellendus similique ut voluptas.</p>
+
+        <img src="images/fox1.png"
+             alt="Thanshuhai - Perfect Blend" class="floatLeft" width="300px" />
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ad aliquid beatae distinctio
+            dolore
+            earum
+            eligendi est, exercitationem expedita labore laudantium magnam molestias numquam obcaecati odit officia,
+            praesentium quia rem sapiente sed similique veniam voluptates voluptatibus. Deleniti, distinctio
+            tempora.
+            Dolor
+            eveniet explicabo maiores nobis temporibus ut veniam, vero. Amet aut autem, dolorem excepturi in ipsam
+            ipsum
+            mollitia nam nesciunt perferendis reprehenderit ullam, veritatis vitae. Ab accusamus architecto autem
+            commodi
+            consequuntur cumque dicta dignissimos distinctio dolore dolorem doloremque ducimus enim eos harum hic
+            illo
+            in
+            inventore, ipsa, itaque libero magni maiores minima nesciunt nulla officia quis ratione sed suscipit
+            tempore
+            totam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid animi architecto ducimus
+            exercitationem fugiat fugit, harum hic illo in, iste itaque labore laudantium magni, necessitatibus
+            praesentium sit suscipit voluptatum.</p>
+
+        <img src="images/fox2.png"
+             alt="Thanshuhai - 404 Coffee Not Found" class="floatRight" width="350px" />
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus excepturi exercitationem
+            fugit,
+            iure omnis pariatur voluptatum. Accusamus ad aliquid, assumenda autem dicta dolorem eum facere, in
+            itaque
+            nam
+            nostrum perspiciatis quae quasi quia quidem reprehenderit, rerum totam ut veritatis voluptatem. Alias
+            culpa
+            doloremque, dolores error et facere, impedit ipsum laboriosam, magnam molestiae nostrum sunt unde vel.
+            Dignissimos ducimus est voluptatibus. Adipisci, cumque dolores error eveniet illum incidunt libero
+            magni,
+            minima
+            molestias quisquam reiciendis reprehenderit repudiandae soluta tenetur vitae. Aliquam commodi
+            consectetur
+            consequuntur cumque delectus, distinctio ducimus ea eius et facere facilis incidunt inventore magni
+            maxime
+            molestiae nostrum odio omnis pariatur placeat provident quaerat qui quisquam, rem repudiandae saepe sed
+            sunt
+            temporibus totam ullam voluptatibus.</p>
+
+        <img src="images/fox3.png"
+             alt="Thanshuhai - A Mug of Fluff" width="300" class="floatLeft" />
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ea iste labore quas quia quos similique
+            tenetur vero. Accusamus architecto at consequuntur cumque dolores doloribus eligendi, fuga hic, maxime natus
+            obcaecati quae quas, quia quo tenetur totam ut voluptate. Animi cumque ea minima non reprehenderit.
+            Accusamus ad asperiores consequatur cumque et excepturi fugit ipsum odit optio quaerat, quas, repellat
+            repudiandae voluptate. Architecto atque facilis, ipsa odit placeat quis ullam voluptas. Deserunt ducimus,
+            expedita facilis itaque maiores non vero. Ad dignissimos doloribus esse eum fuga illum ipsa ipsum maiores
+            maxime necessitatibus nobis nulla numquam quis, rem repellendus unde voluptatem. Earum, vel.</p>
+    </div>
+
+</div>
+<footer>
+
+</footer>
+
+</body>
+</html>
